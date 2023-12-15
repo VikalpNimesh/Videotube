e3aaf7b5ad73ac0e127a8d631cadc359ac7ea05d
vikalp <vikalpnimesh786@gmail.com>
2023-12-15 11:36:11 +0530

    add register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,20 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-const router = Router()
+const router = Router();
 
-router.route("/register").post(registerUser)
-export default router
+// Move the upload middleware into the route definition
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+
+export default router;
